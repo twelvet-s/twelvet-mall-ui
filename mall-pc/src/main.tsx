e1ba77c8457282cf28f0f4ee14688b426790fb9c
TwelveT <2471835953@qq.com>
2023-07-14 09:51:13 +0800

    使用BrowserRouter

--- mall-pc/src/main.tsx
+++ mall-pc/src/main.tsx
@@ -2,12 +2,12 @@ import { Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { HashRouter } from 'react-router-dom'
+import {BrowserRouter} from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <HashRouter>
+  <BrowserRouter>
     <Suspense fallback={<>loading</>}>
       <App />
     </Suspense>
-  </HashRouter>,
+  </BrowserRouter>,
 )
