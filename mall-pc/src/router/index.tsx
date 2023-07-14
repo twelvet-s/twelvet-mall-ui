e9761b1fee836be0be80a24c69375ee239f87418
TwelveT <2471835953@qq.com>
2023-07-14 17:58:02 +0800

    loading

--- mall-pc/src/router/index.tsx
+++ mall-pc/src/router/index.tsx
@@ -1,5 +1,6 @@
 import {lazy, ReactNode, Suspense} from 'react'
 import type {RouteObject} from 'react-router-dom'
+import {Spin} from "antd";
 
 const Layout = lazy(() => import('../layout'))
 const Index = lazy(() => import('../pages/Index'))
@@ -14,7 +15,16 @@ const Test = lazy(() => import('../pages/Test.tsx'))
 const NotFound = lazy(() => import('../pages/Error/404.tsx'))
 
 const lazyLoad = (children: ReactNode) => {
-    return <Suspense fallback={<>loading</>}>
+    return <Suspense fallback={
+        <div style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100vh'
+        }}>
+            <Spin size="large"/>
+        </div>
+    }>
         {children}
     </Suspense>
 }
