9f5be190f274aff52e6c006742620c350fceea19
TwelveT <2471835953@qq.com>
2023-08-29 17:47:16 +0800

    video

--- mall-pc/src/pages/Live/Video/index.tsx
+++ mall-pc/src/pages/Live/Video/index.tsx
@@ -11,25 +11,9 @@ const Video: React.FC<{
 
     const { flvSource } = props
 
-    const videoRef = useRef<HTMLCanvasElement>(null)
+    const videoRef = useRef<HTMLVideoElement>(null)
 
     const [player, setPlayer] = useState<mpegts.Player>()
-
-    // 创建播放器
-    const videoEl = document.createElement('video');
-
-    videoEl.autoplay = true;
-    videoEl.muted = true;
-    videoEl.playsInline = true;
-    videoEl.loop = true;
-    videoEl.controls = true;
-    videoEl.setAttribute('webkit-playsinline', 'true');
-    videoEl.setAttribute('x5-video-player-type', 'h5');
-    videoEl.setAttribute('x5-video-player-fullscreen', 'true');
-    videoEl.setAttribute('x5-video-orientation', 'portraint');
-    videoEl.oncontextmenu = (e) => {
-        e.preventDefault();
-    };
 
     /**
      * 全屏播放
@@ -65,21 +49,6 @@ const Video: React.FC<{
 
     }, [])
 
-    /**
-     * 渲染canvas
-     */
-    const renderCanvas = () => {
-        if (videoRef.current) {
-            const w = 1066;
-            const h = 750;
-            videoRef.current.width = w;
-            videoRef.current.height = h;
-            const ctx = videoRef.current.getContext('2d')!
-            ctx.drawImage(videoEl, 0, 0, w, h)
-        }
-        requestAnimationFrame(renderCanvas)
-    }
-
     useEffect(() => {
         if (flvSource && mpegts.getFeatureList().mseLivePlayback && videoRef && videoRef.current) {
             const playerMpegts = mpegts.createPlayer({
@@ -87,13 +56,10 @@ const Video: React.FC<{
                 isLive: true,
                 url: flvSource
             });
-
-
-            playerMpegts.attachMediaElement(videoEl);
+            playerMpegts.attachMediaElement(videoRef.current);
             playerMpegts.load();
             const play = playerMpegts.play()
 
-            renderCanvas()
             setPlayer(playerMpegts);
         }
     }, [flvSource])
@@ -110,8 +76,7 @@ const Video: React.FC<{
 
     return (
         <>
-            {/* <video controls autoPlay={false} ref={videoRef} style={{ width: '100%', height: '100%' }} /> */}
-            <canvas ref={videoRef}></canvas>
+            <video controls autoPlay={false} ref={videoRef} style={{ width: '100%', height: '100%' }} />
         </>
     )
 }
