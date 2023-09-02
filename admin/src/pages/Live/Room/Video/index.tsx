569e05b4fefff22a3f94908e727111d60db4c60c
TwelveT <2471835953@qq.com>
2023-09-02 17:50:12 +0800

    webrtc推流

--- admin/src/pages/Live/Room/Video/index.tsx
+++ admin/src/pages/Live/Room/Video/index.tsx
@@ -15,21 +15,21 @@ const Video: React.FC<VideoProps> = porps => {
     const videoRef = useRef<HTMLVideoElement>(null);
 
     const rtcPeerConnection = new RTCPeerConnection();
-    const mediaStream = new MediaStream();
+    //const mediaStream = new MediaStream();
 
 
 
     // 使用webrtc发送数据
-    const startScreenSharing = async (stream: MediaProvider) => {
+    const startScreenSharing = async (stream: MediaStream) => {
         if (videoRef.current) {
-            const tid = 1;
             rtcPeerConnection.addTransceiver("audio", { direction: "sendonly" });
             rtcPeerConnection.addTransceiver("video", { direction: "sendonly" });
-            //rtcPeerConnection.addTransceiver("video", {direction: "sendonly"});
-            //rtcPeerConnection.addTransceiver("audio", {direction: "sendonly"});
 
+            // 本地播放
             videoRef.current.srcObject = stream;
             videoRef.current.play();
+
+            // 添加流
             // @see https://developer.mozilla.org/en-US/docs/Web/API/RTCPeerConnection/addStream#Migrating_to_addTrack
             stream.getTracks().forEach(function (track: MediaStreamTrack) {
                 rtcPeerConnection.addTrack(track);
@@ -38,21 +38,17 @@ const Video: React.FC<VideoProps> = porps => {
                 //mediaStream.addTrack(track);
             });
 
-            let offer = await rtcPeerConnection.createOffer();
+            const offer = await rtcPeerConnection.createOffer();
+
             await rtcPeerConnection.setLocalDescription(offer);
+
             // 发送数据
-            let session: {
+            const session: {
                 sdp: string,
             } = await push({
-                // 自定义直播token
-                token: tid,
-                // webrtc规范主要
-                streamurl: "webrtc://localhost/live/livestream/1",
-                clientip: null,
                 sdp: offer.sdp
             })
 
-            console.log('=======', session)
             if (session) {
                 await rtcPeerConnection.setRemoteDescription(
                     new RTCSessionDescription({ type: 'answer', sdp: session.sdp })
@@ -69,7 +65,7 @@ const Video: React.FC<VideoProps> = porps => {
         if (liveType === 'video') { // 视频直播
             if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
                 // 获取用户媒体设备的权限
-                const stream = navigator.mediaDevices.getUserMedia({
+                const stream = await navigator.mediaDevices.getUserMedia({
                     video: true
                 })
                 startScreenSharing(stream)
