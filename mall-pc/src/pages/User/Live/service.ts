c294661535f97b073eeceb8be56ab6f00db8c828
TwelveT <2471835953@qq.com>
2023-09-05 15:19:13 +0800

    用户发起直播

--- mall-pc/src/pages/User/Live/service.ts
+++ mall-pc/src/pages/User/Live/service.ts
@@ -1,12 +1,12 @@
 
 
-// 请求的控制器名称
-const controller = "/live";
+// // 请求的控制器名称
+// const controller = "/live";
 
-/**
- * 发起直播
- * @param params 搜索参数
- */
-export async function push(params: Record<string, any>) {
-    return 132
-}
+// /**
+//  * 发起直播
+//  * @param params 搜索参数
+//  */
+// export async function push(params: Record<string, any>) {
+//     return 132
+// }
