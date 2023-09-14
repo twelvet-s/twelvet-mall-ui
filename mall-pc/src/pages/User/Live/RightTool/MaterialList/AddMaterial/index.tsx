2c68175212e8ec26e466dcac9b3a63147a10edbe
TwelveT <2471835953@qq.com>
2023-09-14 18:15:19 +0800

    添加素材

--- mall-pc/src/pages/User/Live/RightTool/MaterialList/AddMaterial/index.tsx
+++ mall-pc/src/pages/User/Live/RightTool/MaterialList/AddMaterial/index.tsx
@@ -1,51 +1,66 @@
-import { AudioOutlined, DesktopOutlined, FolderOpenOutlined, FolderViewOutlined, FontSizeOutlined, PictureOutlined, VideoCameraOutlined } from '@ant-design/icons';
-import { Button, Card, Form, List, Modal } from 'antd';
 import React from 'react'
+import { AudioOutlined, DesktopOutlined, FolderOpenOutlined, FolderViewOutlined, FontSizeOutlined, PictureOutlined, VideoCameraOutlined } from '@ant-design/icons'
+import { Button, Checkbox, ColorPicker, Form, Input, List, Modal, Select, Slider, Upload } from 'antd'
+import styles from './style.module.css'
+import { MaterialTypeEnum } from './interface'
 
 
 // 添加素材
 const AddMateral: React.FC = () => {
 
+    const formItemLayout = {
+        labelCol: {
+            sm: { span: 6 },
+        },
+        wrapperCol: {
+            sm: { span: 16 },
+        },
+    };
+
     // 选择直播素材
     const [materialSelectModal, setMaterialSelectModal] = React.useState<boolean>(false)
 
+    // 当前素材组件类型
+    const [materialComponentType, setMaterialComponentType] = React.useState<string>()
+
+    // 素材列表
     const materialSelect = [
         {
-            type: 'camera',
-            icon: <FolderViewOutlined />,
+            type: MaterialTypeEnum.CAMERA,
+            icon: <FolderViewOutlined className={styles.icons} />,
             title: '摄像头',
         },
         {
-            type: 'audio',
-            icon: <AudioOutlined />,
+            type: MaterialTypeEnum.AUDIO,
+            icon: <AudioOutlined className={styles.icons} />,
             title: '音频',
         },
         {
-            type: 'window',
-            icon: <DesktopOutlined />,
+            type: MaterialTypeEnum.SCREEN,
+            icon: <DesktopOutlined className={styles.icons} />,
             title: '窗口',
         },
         {
-            type: 'picture',
-            icon: <PictureOutlined />,
+            type: MaterialTypeEnum.PICTURE,
+            icon: <PictureOutlined className={styles.icons} />,
             title: '图片',
         },
         {
-            type: 'text',
-            icon: <FontSizeOutlined />,
+            type: MaterialTypeEnum.TEXT,
+            icon: <FontSizeOutlined className={styles.icons} />,
             title: '文本',
         },
         {
-            type: 'video',
-            icon: <VideoCameraOutlined />,
+            type: MaterialTypeEnum.VIDEO,
+            icon: <VideoCameraOutlined className={styles.icons} />,
             title: '多媒体',
         },
         {
-            type: 'folder',
-            icon: <FolderOpenOutlined />,
+            type: MaterialTypeEnum.FOLDER,
+            icon: <FolderOpenOutlined className={styles.icons} />,
             title: '相册',
         }
-    ];
+    ]
 
     // 填写直播素材信息
     const [materialModal, setMaterialModal] = React.useState<boolean>(false)
@@ -53,12 +68,387 @@ const AddMateral: React.FC = () => {
     const [form] = Form.useForm<{ name: string; company: string }>()
 
     // 处理选择的直播素材
-    const handleSelectMaterial = (type: string) => {
-        setMaterialModal(true)
-        setMaterialSelectModal(false)
-        if (type === 'text') {
-
+    const genMaterial = () => {
+        switch (materialComponentType) {
+            case MaterialTypeEnum.CAMERA:
+                return camareMaterial()
+            case MaterialTypeEnum.AUDIO:
+                return audioMaterial()
+            case MaterialTypeEnum.SCREEN:
+                return screenMaterial()
+            case MaterialTypeEnum.PICTURE:
+                return pictureMaterial()
+            case MaterialTypeEnum.TEXT:
+                return textMaterial()
+            case MaterialTypeEnum.VIDEO:
+                return videoMaterial()
+            case MaterialTypeEnum.FOLDER:
+                return folderMaterial()
         }
+    }
+
+    // 摄像头组件
+    const camareMaterial = () => {
+        return (
+            <>
+                <Form.Item
+                    {...formItemLayout}
+                    label="设备选择"
+                    name="camare"
+                    rules={[{ required: true, message: '内容为空' }]}
+                >
+                    <Select
+                        defaultValue="lucy"
+                        allowClear
+                        options={[{ value: 'lucy', label: '微软雅黑' }]}
+                    />
+                </Form.Item>
+
+                <Form.Item
+                    {...formItemLayout}
+                    label="来源命名"
+                    name="name"
+                    rules={[{ required: true, message: '来源命名不能为空' }]}
+                >
+                    <Input placeholder="来源命名" />
+                </Form.Item>
+            </>
+        )
+    }
+
+    // 音频组件
+    const audioMaterial = () => {
+        return (
+            <>
+                <Form.Item
+                    {...formItemLayout}
+                    label="设备选择"
+                    name="audio"
+                    rules={[{ required: true, message: '内容为空' }]}
+                >
+                    <Select
+                        defaultValue="lucy"
+                        allowClear
+                        options={[{ value: 'lucy', label: '微软雅黑' }]}
+                    />
+                </Form.Item>
+
+                <Form.Item
+                    {...formItemLayout}
+                    label="来源命名"
+                    name="name"
+                    rules={[{ required: true, message: '来源命名不能为空' }]}
+                >
+                    <Input placeholder="来源命名" />
+                </Form.Item>
+            </>
+        )
+    }
+
+    // 窗口组件
+    const screenMaterial = () => {
+        return (
+            <>
+                <Form.Item
+                    {...formItemLayout}
+                    label="来源命名"
+                    name="name"
+                    rules={[{ required: true, message: '来源命名不能为空' }]}
+                >
+                    <Input placeholder="来源命名" />
+                </Form.Item>
+            </>
+        )
+    }
+
+    // 图片组件
+    const pictureMaterial = () => {
+        return (
+            <>
+                <Form.Item
+                    {...formItemLayout}
+                    label="来源命名"
+                    name="name"
+                    rules={[{ required: true, message: '来源命名不能为空' }]}
+                >
+                    <Input placeholder="来源命名" />
+                </Form.Item>
+
+                <Form.Item
+                    {...formItemLayout}
+                    label="添加本地图片"
+                    name="picture"
+                    rules={[{ required: true, message: '来源命名不能为空' }]}
+                >
+                    <Upload
+                        name="avatar"
+                        listType="picture-card"
+                        className="avatar-uploader"
+                        showUploadList={false}
+                        action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+                    >
+                    </Upload>
+                </Form.Item>
+
+                <Form.Item
+                    {...formItemLayout}
+                    label="不透明度"
+                    name="opacity"
+                    initialValue={100}
+                >
+                    <Slider
+                        min={1}
+                        max={100}
+                    />
+                </Form.Item>
+            </>
+        )
+    }
+
+    // 文字组件
+    const textMaterial = () => {
+        return (
+            <>
+                <Form.Item
+                    {...formItemLayout}
+                    label="来源命名"
+                    name="name"
+                    rules={[{ required: true, message: '来源命名不能为空' }]}
+                >
+                    <Input placeholder="来源命名" />
+                </Form.Item>
+                <Form.Item
+                    {...formItemLayout}
+                    label="内容"
+                    name="text"
+                    rules={[{ required: true, message: '内容为空' }]}
+                >
+                    <Input.TextArea placeholder="内容" />
+                </Form.Item>
+
+                <Form.Item
+                    {...formItemLayout}
+                    label="字体样式"
+                >
+                    <Form.Item
+                        name="fontFamily"
+                        style={{ display: 'inline-block', width: 'calc(35% - 5px)' }}
+                        initialValue={'Microsoft YaHei'}
+                    >
+                        <Select
+                            defaultValue="lucy"
+                            style={{ width: 95 }}
+                            allowClear
+                            options={[{ value: 'Microsoft YaHei', label: '微软雅黑' }]}
+                        />
+                    </Form.Item>
+
+                    <Form.Item
+                        name="fontSize"
+                        style={{ display: 'inline-block', width: 'calc(22% - 5px)' }}
+                        initialValue={32}
+                    >
+                        <Select
+                            style={{ width: 55 }}
+                            allowClear
+                            options={[
+                                { value: 12, label: '12' },
+                                { value: 14, label: '14' },
+                                { value: 16, label: '16' },
+                                { value: 18, label: '18' },
+                                { value: 20, label: '20' },
+                                { value: 24, label: '24' },
+                                { value: 28, label: '28' },
+                                { value: 32, label: '32' },
+                                { value: 36, label: '36' },
+                                { value: 40, label: '40' },
+                            ]}
+                        />
+                    </Form.Item>
+
+                    <Form.Item
+                        name="fontStyle"
+                        style={{ display: 'inline-block', width: 'calc(30% - 5px)' }}
+                        initialValue={12}
+                    >
+                        <Select
+                            style={{ width: 82 }}
+                            allowClear
+                            options={[
+                                { value: 'default', label: '默认' },
+                                { value: 'italic', label: '斜体' },
+                                { value: 'weight', label: '粗体' },
+                                { value: 'italicAndWeight', label: '粗斜体' }
+                            ]}
+                        />
+                    </Form.Item>
+                    <Form.Item
+                        name="color"
+                        style={{ display: 'inline-block', width: 'calc(13% - 5px)' }}
+                        initialValue={'#ffffff'}
+                    >
+                        <ColorPicker />
+                    </Form.Item>
+                </Form.Item>
+
+                <Form.Item
+                    {...formItemLayout}
+                    label="描边"
+                >
+                    <Form.Item
+                        name="textStroke"
+                        style={{ display: 'inline-block', width: 'calc(32% - 12px)' }}
+                    >
+                        <Checkbox value={1}>
+                            启用描边
+                        </Checkbox>
+                    </Form.Item>
+
+                    <Form.Item
+                        name="textStrokeColor"
+                        style={{ display: 'inline-block', width: 'calc(32% - 12px)' }}
+                        initialValue={'#ffffff'}
+                    >
+                        <ColorPicker />
+                    </Form.Item>
+
+                    <Form.Item
+                        name="textStrokeWeight"
+                        initialValue={2}
+                    >
+                        <Slider
+                            min={1}
+                            max={20}
+                        />
+                    </Form.Item>
+                </Form.Item>
+
+                <Form.Item
+                    {...formItemLayout}
+                    label="不透明度"
+                    name="opacity"
+                    initialValue={100}
+                >
+                    <Slider
+                        min={1}
+                        max={100}
+                        value={0}
+                    />
+                </Form.Item>
+            </>
+        )
+    }
+
+    // 媒体组件
+    const videoMaterial = () => {
+        return (
+            <>
+                <Form.Item
+                    {...formItemLayout}
+                    label="来源命名"
+                    name="name"
+                    rules={[{ required: true, message: '来源命名不能为空' }]}
+                >
+                    <Input placeholder="来源命名" />
+                </Form.Item>
+
+                <Form.Item
+                    {...formItemLayout}
+                    label="多媒体文件"
+                    name="video"
+                    rules={[{ required: true, message: '来源命名不能为空' }]}
+                >
+                    <Upload
+                        name="avatar"
+                        listType="picture-card"
+                        className="avatar-uploader"
+                        showUploadList={false}
+                        action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+                    >
+                    </Upload>
+                </Form.Item>
+            </>
+        )
+    }
+
+    // 相册组件
+    const folderMaterial = () => {
+        return (
+            <>
+                <Form.Item
+                    {...formItemLayout}
+                    label="来源命名"
+                    name="name"
+                    rules={[{ required: true, message: '来源命名不能为空' }]}
+                >
+                    <Input placeholder="来源命名" />
+                </Form.Item>
+
+                <Form.Item
+                    {...formItemLayout}
+                    label="添加本地图片"
+                    name="pictures"
+                    rules={[{ required: true, message: '来源命名不能为空' }]}
+                >
+                    <Upload
+                        name="avatar"
+                        listType="picture-card"
+                        className="avatar-uploader"
+                        showUploadList={false}
+                        action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+                    >
+                    </Upload>
+                </Form.Item>
+
+
+                <Form.Item
+                    {...formItemLayout}
+                    label="动画效果"
+                    name="animation"
+                    initialValue={'fade'}
+                >
+                    <Select
+                        style={{ width: 100 }}
+                        allowClear
+                        options={[
+                            { value: 'fade', label: '淡入淡出' },
+                            { value: 'toLeft', label: '向左滑动' },
+                            { value: 'toRight', label: '向左退出' }
+                        ]}
+                    />
+                </Form.Item>
+
+                <Form.Item
+                    {...formItemLayout}
+                    label="showTime "
+                    name="name"
+                    initialValue={3000}
+                >
+                    <Slider
+                        min={100}
+                        max={10000}
+                    />
+                </Form.Item>
+
+                <Form.Item
+                    {...formItemLayout}
+                    label="过渡时间"
+                    name="transitionTime"
+                    initialValue={1000}
+                >
+                    <Slider
+                        min={100}
+                        max={10000}
+                    />
+                </Form.Item>
+            </>
+        )
+    }
+
+    // 发送素材
+    const handleSendtMaterial = () => {
+        setMaterialModal(false)
+        setMaterialSelectModal(false)
     }
 
     return (
@@ -67,6 +457,7 @@ const AddMateral: React.FC = () => {
 
             <Modal
                 title={'添加直播素材'}
+                centered={true}
                 open={materialSelectModal}
                 onCancel={() => setMaterialSelectModal(false)}
                 footer
@@ -75,13 +466,20 @@ const AddMateral: React.FC = () => {
                     grid={{ gutter: 16, column: 5 }}
                     dataSource={materialSelect}
                     renderItem={(item) => (
-                        <List.Item>
-                            <Button onClick={() => handleSelectMaterial(item.type)}>
-                                {
-                                    item.icon
-                                }
-                                {item.title}
-                            </Button>
+                        <List.Item
+                            onClick={() => {
+                                setMaterialModal(true)
+                                setMaterialSelectModal(false)
+                                setMaterialComponentType(item.type)
+                            }}
+                            style={{
+                                marginBlockEnd: 0
+                            }}
+                        >
+                            <div className={styles.materialListCtn}>
+                                {item.icon}
+                                <p>{item.title}</p>
+                            </div>
                         </List.Item>
                     )}
                 />
@@ -90,12 +488,20 @@ const AddMateral: React.FC = () => {
             <Modal
                 open={materialModal}
                 title={'添加直播素材'}
+                centered={true}
+                onOk={() => {
+                    handleSendtMaterial()
+                }}
                 onCancel={() => {
                     setMaterialSelectModal(false)
                     setMaterialModal(false)
                 }}
+                okText={'发送'}
+                cancelText={'取消'}
             >
-
+                <Form name="material" form={form}>
+                    {genMaterial()}
+                </Form>
             </Modal>
         </>
     )
---
+++ mall-pc/src/pages/User/Live/RightTool/MaterialList/AddMaterial/interface.ts
@@ -0,0 +1,9 @@
+export enum MaterialTypeEnum {
+    CAMERA  = 'camera',
+    AUDIO   = 'audio',
+    SCREEN  = 'screen',
+    PICTURE = 'picture',
+    TEXT    = 'text',
+    VIDEO   = 'video',
+    FOLDER  = 'folder',
+}
